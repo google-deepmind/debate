24337172e6addd3ded60d37c6d4ccc701f165ad7
Geoffrey Irving <irving@naml.us>
2024-08-14 20:53:17 +0100

    Change 4.8.0-rc2 to 4.8.0

--- lakefile.lean
+++ lakefile.lean
@@ -9,7 +9,7 @@ package debate where
     ⟨`relaxedAutoImplicit, false⟩
   ]
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.8.0"
 
 @[default_target]
 lean_lib Debate
